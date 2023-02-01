5b2570d7363417b1e32eca89f803e0ebda528525
Mário Garrido <1191102+mariogarridopt@users.noreply.github.com>
2023-02-01 23:59:44 +0000

    update repo name

--- awselect.rb
+++ awselect.rb
@@ -1,7 +1,7 @@
 class Awselect < Formula
   desc "Plugin that enables fzf seach on awsume"
   homepage "https://github.com/mariogarridopt/awsume-fzf"
-  url "https://github.com/mariogarridopt/awsume-fzf/archive/refs/tags/v1.0.tar.gz"
+  url "https://github.com/mariogarridopt/homebrew-awsume-fzf/archive/refs/tags/v1.0.tar.gz"
   sha256 "727f2ae516070a7fdde15043065a6099bf195ff7751c507fa205650e64ee68de"
   license :public_domain
 
