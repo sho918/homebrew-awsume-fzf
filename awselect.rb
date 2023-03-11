78fbfb8d1fcf35fa8276d50bdf83ae2ca83141ff
Arai Shota <me@arsho.dev>
2023-03-11 18:10:17 +0900

    Update awselect.rb

--- awselect.rb
+++ awselect.rb
@@ -2,7 +2,7 @@ class Awselect < Formula
   desc "Plugin that enables fzf seach on awsume"
   homepage "https://github.com/mariogarridopt/awsume-fzf"
   url "https://github.com/mariogarridopt/homebrew-awsume-fzf/archive/refs/tags/v1.0.tar.gz"
-  sha256 "727f2ae516070a7fdde15043065a6099bf195ff7751c507fa205650e64ee68de"
+  sha256 "97df1059f03a79bc3d57c1fe324c999cceddde31e0ece1e5b3861d767856bf31"
   license :public_domain
 
   depends_on "awscli"
